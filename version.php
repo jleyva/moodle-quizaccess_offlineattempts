114e194cb8ec55c52ca0561c128fdc6a044ad5b8
Juan Leyva <juanleyvadelgado@gmail.com>
2016-05-13 16:47:33 +0200

    MOBILE-1579: Fix typo in verison.php

--- version.php
+++ version.php
@@ -31,4 +31,4 @@ $plugin->dependencies = array(
     'local_mobile' => 2014060325
 );
 $plugin->release   = 'v2.6-3.1-1';
-$plugin->madurity  = MATURITY_STABLE;
+$plugin->maturity  = MATURITY_STABLE;
